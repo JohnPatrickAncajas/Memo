67a6e5c9741d45218893c66660538d60f8a5c854
JohnPatrickAncajas <ancajasjohnpatrickdelapena@gmail.com>
2023-11-08 18:35:05 +0800

    Added Notes
    Added the basic logic of swapping between Home and Notes.
    
    Added comments for redability.
    Improved CSS for better design.
    Changed HTML for better content.

--- script.js
+++ script.js
@@ -1,10 +1,88 @@
-function getCurrentYear() {
+// Boolean declarations
 
-    const currentYear = document.getElementById('currentYear');
-    const currentDate = new Date();
-    const currentYearValue = currentDate.getFullYear();
+let isShownHome = true;
+let isShownNotes = false;
+
+// Element declarations CONST
+
+const buttonHome = document.getElementById('buttonHome');
+const buttonNotes = document.getElementById('buttonNotes');
+const body = document.querySelector('body');
+const home = document.getElementById('home');
+const footer = body.children[1];
+
+// Element declarations LET
+
+let notes = document.querySelectorAll('.notes');
+
+// Event listeners
+
+buttonHome.addEventListener('click', function() {
+
+    if (isShownHome == false) {
+
+        console.log("Welcome to home!");
+
+        home.style.display = 'block';
+
+        notes.forEach(function(element) {
+            element.style.display = 'none';
+        });
+
+        isShownHome = true;
+        isShownNotes = false;
+
+    } else {
+        console.log("Your at home already!");
+    }
+});
+
+buttonNotes.addEventListener('click', function() {
+
+    if (isShownNotes == false) {
+
+        home.style.display = 'none';
+
+        const newDiv = document.createElement('div');
+
+        newDiv.classList.add('notes');
+
+        newDiv.textContent = 'This is the data saved at local storage';
+
+        body.insertBefore(newDiv, footer);
+
+        notes = document.querySelectorAll('.notes');
+
+        isShownNotes = true;
+        isShownHome = false;
+
+    } else {
+
+        const newDiv = document.createElement('div');
+
+        newDiv.classList.add('notes');
+
+        newDiv.textContent = 'This is new data created.';
+
+        body.insertBefore(newDiv, footer);
+
+        notes = document.querySelectorAll('.notes');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Automatically updates year at footer
+
+    function getCurrentYear() {
+
+        const currentYear = document.getElementById('currentYear');
+        const currentDate = new Date();
+        const currentYearValue = currentDate.getFullYear();
+    
+        currentYear.innerText = currentYearValue.toString();
+    }
   
-    currentYear.innerText = currentYearValue.toString();
-}
-
-getCurrentYear();
+    getCurrentYear();
+  });
+  
