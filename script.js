cd31e58ff0951e403cd69768ece45e4de68e13e9
JohnPatrickAncajas <ancajasjohnpatrickdelapena@gmail.com>
2023-11-17 12:05:23 +0800

    Improved Empty Input Logic
    Adjusted the JS logic that validates the user input to make sure it have values for all three forms before adding it as a note.
    
    Added one more base note.

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const notesClearButton = document.getElementById('notesClearButton');
 // Element declarations LET
 
 let notesDiv = document.querySelectorAll('.notes');
-let notesList = [["Christmass!", "December 25, 2023", "Celebrate the christmass holiday and enjoy!"], ["New Year!", "January 1, 2024", "Have fun and celebrate the new year!"], ["Death.", "Soon.", "Make each moment count."]];
+let notesList = [["Christmass", "December 25", "Celebrate the christmass holidays and enjoy!"], ["New Year", "January 1", "Have fun and celebrate the new year!"], ["Valentines", "February 14", "Share the love at this special day!"], ["Death", "Soon", "Make each moment count."]];
 let notesListString = JSON.stringify(notesList);
 
 // Data declarations
@@ -122,56 +122,64 @@ notesAddButton.addEventListener('click', function() {
         const notesTitleInputValue = notesTitleInput.value;
         const notesDateInputValue = notesDateInput.value;
         const notesTaskInputValue = notesTaskInput.value;
+        
+        if (notesTitleInputValue != '' && notesDateInputValue != '' && notesTaskInputValue != '') {
 
-        if (notesTitleInputValue == '' || notesDateInputValue == '' || notesTaskInputValue == '') {
+            // Add Notes when Input is complete
+
+            notesDiv.forEach(function(element) {
+                element.remove();
+            });
+    
+            const data = localStorage.getItem('data');
+    
+            const currentArray = JSON.parse(data);
+    
+            currentArray.push([notesTitleInputValue, notesDateInputValue, notesTaskInputValue]);
+    
+            currentArray.forEach(value => {
+    
+                const newDiv = document.createElement('div');
+                const newDivTitleAndDate = document.createElement('div');
+                const newPTitle = document.createElement("p");
+                const newPDate = document.createElement("p");
+                const newPTask = document.createElement("p");
+                const hr = document.createElement("hr");
+    
+                newDiv.classList.add('notes');
+                newDivTitleAndDate.classList.add('notesTitleAndDate');
+                newPTask.classList.add('notesTask');
+    
+                newPTitle.textContent = value[0];
+                newPDate.textContent = value[1];
+                newPTask.textContent = value[2];
+    
+                newDivTitleAndDate.appendChild(newPTitle);
+                newDivTitleAndDate.appendChild(newPDate);
+    
+                newDiv.appendChild(newDivTitleAndDate);
+                newDiv.appendChild(hr);
+                newDiv.appendChild(newPTask);
+    
+                body.insertBefore(newDiv, footer);
+            });
+    
+            const updatedArrayString = JSON.stringify(currentArray);
+            localStorage.setItem('data', updatedArrayString);
+    
+            notesDiv = document.querySelectorAll('.notes');
+    
+            notesTitleInput.value = '';
+            notesDateInput.value = '';
+            notesTaskInput.value = '';
+
+        } else {
+
+            // Alert when Input is Incomplete
+
             alert('You need to complete all forms!');
-        } 
 
-        notesDiv.forEach(function(element) {
-            element.remove();
-        });
-
-        const data = localStorage.getItem('data');
-
-        const currentArray = JSON.parse(data);
-
-        currentArray.push([notesTitleInputValue, notesDateInputValue, notesTaskInputValue]);
-
-        currentArray.forEach(value => {
-
-            const newDiv = document.createElement('div');
-            const newDivTitleAndDate = document.createElement('div');
-            const newPTitle = document.createElement("p");
-            const newPDate = document.createElement("p");
-            const newPTask = document.createElement("p");
-            const hr = document.createElement("hr");
-
-            newDiv.classList.add('notes');
-            newDivTitleAndDate.classList.add('notesTitleAndDate');
-            newPTask.classList.add('notesTask');
-
-            newPTitle.textContent = value[0];
-            newPDate.textContent = value[1];
-            newPTask.textContent = value[2];
-
-            newDivTitleAndDate.appendChild(newPTitle);
-            newDivTitleAndDate.appendChild(newPDate);
-
-            newDiv.appendChild(newDivTitleAndDate);
-            newDiv.appendChild(hr);
-            newDiv.appendChild(newPTask);
-
-            body.insertBefore(newDiv, footer);
-        });
-
-        const updatedArrayString = JSON.stringify(currentArray);
-        localStorage.setItem('data', updatedArrayString);
-
-        notesDiv = document.querySelectorAll('.notes');
-
-        notesTitleInput.value = '';
-        notesDateInput.value = '';
-        notesTaskInput.value = '';
+        }
 });
 
 
